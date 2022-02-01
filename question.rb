22c4b696318906334268804c7776e7600ac532c8
Andrew <andrew.spon@gmail.com>
2022-02-01 19:52:28 +0000

    finish question.rb

--- question.rb
+++ question.rb
@@ -1,20 +1,24 @@
-class question
+class Question
   
-  num1 = rand(1..20)
-  num2 = rand(1..20)
-  answer = num1 + num2
+  attr_reader :num1, :num2, :answer
   
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = num1 + num2
+  end
   
   def ask_question(player)
     puts "What is #{num1} + #{num2} ?"
     
     if gets.chomp != answer
-    incorrect
+      incorrect(player)
+    end
+    
   end
   
-  def incorrect
-    #lose_life
+  def incorrect(player)    
+    player.lose_life
   end
-  
   
 end
